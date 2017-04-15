2854468826b764d5cd9935d2aa868e6ca2afd5c4
DarkHunterG <henri.guimaraes2015@gmail.com>
2017-04-14 22:11:09 -0300

    Add files via upload

---
+++ ClientePF.java
@@ -0,0 +1,33 @@
+
+package ClientePF;
+
+public class ClientePF {
+    private String nome;
+    private String cpf;
+    private int idade = 0;
+    
+    //construtor
+    public ClientePF(String nome,int idade,String cpf){
+    this.nome = nome;
+    this.idade = idade;
+    this.cpf = cpf;
+    }
+    public ClientePF(String nome){
+    this.nome = nome;
+    }
+    public String getNome(){
+    return nome;
+    }
+    public void setCpf(String cpf){
+    this.cpf = cpf;
+    }
+    public String getCpf(){
+    return cpf;
+    }
+    public void setIdade(int idade){
+    this.idade = idade;
+    }
+    public int getIdade(){
+    return idade;
+    }
+}
